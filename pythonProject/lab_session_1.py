ce6673f56b342695e80d45f1c0b77ffb022c6d26
beastx03 <114742075+beastx03@users.noreply.github.com>
2022-09-30 13:57:17 +0100

    Add files via upload
    Portfolio 1:
    task 1 is simple hello worlds
    task 2 is hello "name"
    task 3 is converting from Celsius to Fahrenheit
    task 4 is calculating groups and left overs of classes

---
+++ pythonProject/lab_session_1.py
@@ -0,0 +1,22 @@
+#task1
+print('Hello World')
+print('________________________________________________')
+#task2
+name = 'Ahmed'
+print('Hello', name)
+print('________________________________________________')
+#task3
+print('The current temperature in Yorkshire is 38.4C')
+print('Calculating temperature in Fahrenheit....')
+print("Temperature in Fahrenheit is", int(38.4 * 9/5 + 32))
+print('________________________________________________')
+#task4
+print('Batting average is', int(48426/852))
+print('________________________________________________')
+#task5
+print ('1: ', 113//24, 'groups')
+print ('left overs', 113 % 24)
+print ('2: ', 175//24, 'groups')
+print ('left overs', 175 % 24)
+print ('3: ', 12//24, 'groups')
+print ('left overs', 12 % 24)
